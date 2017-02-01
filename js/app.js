230647160b91db494c7fb9a65c22d8eab8878075
Munadil Fahad <munadil98@gmail.com>
2017-02-01 18:27:54 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,49 @@
 
+$(document).ready(function(){
+	
+	var i=0;
+	var j=0;
+
+
+	$("#btn").click(function(){
+
+		var name=$("#name").val();
+
+		if(name==""){
+			$("#pname").text("Name required")
+
+			}
+			else{
+				$("#pname").text("");
+				i=1;
+
+			}
+
+
+	
+	
+
+		var password=$("#password").val();
+		var passlength=password.length;
+
+		if(passlength<6){
+
+			$("#ppass").text("Password must be minimum 6 character long");
+			}
+			else{
+
+				$("#ppass").text("");
+				j=1;
+			}
+
+
+		if(i==1&&j==1){
+			$("#success").text("Successful");
+
+			}
+
+	});	
+
+
+
+});
